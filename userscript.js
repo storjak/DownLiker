0e5daa74ed33051c6d9729e90ffe069f193de999
windowpuncher <jakestorkamp@yahoo.com>
2022-01-27 18:42:50 -0600

    Bugfix, axios was removed so it would not load

--- userscript.js
+++ userscript.js
@@ -29,26 +29,22 @@ const newStyle = document.createElement("style");
 newStyle.innerHTML = ".switch{position:relative;display:inline-block;width:60px;height:34px;}.switch input{opacity:0;width:0;height:0;}.slider{position:absolute;cursor:pointer;top:0;left:0;right:0;bottom:0;background-color:#ccc;-webkit-transition:.4s;  transition:.4s;}.slider:before{position:absolute;content:\"\";height:26px;width:26px;left:4px;bottom:4px;background-color:white;-webkit-transition:.4s;transition:.4s;}input:checked+.slider{background-color:#2196F3;}input:focus+.slider{box-shadow:0 0 1px #2196F3;}input:checked+.slider:before{-webkit-transform:translateX(26px);-ms-transform:translateX(26px);transform:translateX(26px);}.slider.round{border-radius:34px;}.slider.round:before{border-radius:50%;}"; // slider credit goes to w3schools.com
 
 (() => {
-	if (typeof axios === "function") {
-		let detector = setInterval(() => {
-			let checkerHook = document.getElementsByClassName(latestBarClasses);
-			if (checkerHook[0] == null || checkerHook[0] == undefined) {
-				// do nothing
-			} else {
-				clearInterval(detector);
-				clearTimeout(detectorIntervalCleaner);
-				latestBar = document.getElementsByClassName(latestBarClasses);
-				nativeSwitcherButton = document.getElementsByClassName(switcherButtonClasses);
-				initialToggleEmbed();
-			}
-		}, 200);
-		let detectorIntervalCleaner = setTimeout(() => {
-			clearInterval(detector)
-			console.error("DownLiker error: Feed element not detected, could not make hook, likes are not being monitored.");
-		}, 10000);
-	} else {
-		console.error("DownLiker did not load, as the Axios library isn't loading.");
-	}
+	let detector = setInterval(() => {
+		let checkerHook = document.getElementsByClassName(latestBarClasses);
+		if (checkerHook[0] == null || checkerHook[0] == undefined) {
+			// do nothing
+		} else {
+			clearInterval(detector);
+			clearTimeout(detectorIntervalCleaner);
+			latestBar = document.getElementsByClassName(latestBarClasses);
+			nativeSwitcherButton = document.getElementsByClassName(switcherButtonClasses);
+			initialToggleEmbed();
+		}
+	}, 200);
+	let detectorIntervalCleaner = setTimeout(() => {
+		clearInterval(detector)
+		console.error("DownLiker error: Feed element not detected, could not make hook, likes are not being monitored.");
+	}, 10000);
 })();
 
 function initialToggleEmbed() {
